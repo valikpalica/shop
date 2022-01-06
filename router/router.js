f14c73387e29e3ca633af832510ee41454f0c321
valikpalica <valentinpalica@gmail.com>
2022-01-06 11:47:19 +0200

    change route

--- router/router.js
+++ router/router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Controller = require('../API/Controller/controller');
 
 router.get('/getAllGoods',Controller.getAllGoods);
-router.post('/getGoodById',Controller.getGoodById);
+router.post('/getGoodByType',Controller.getGoodById);
 router.post('/setGood',Controller.setGood);
 
 module.exports = router
